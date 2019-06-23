5f1b382729e07680ba0157356da4c186d373ace8
JEverett <jonathan@jonathaneverettcreative.com>
2019-06-23 00:25:15 -0500

    Created first of two complementary components to hold info and an image on the landing page, accepting props for that data. Built a .json file to hold the data for those props. Still need to finish the json file after finding images, as well as the second component. Then flesh out the Landing page.

--- client/src/components/LandingHero/index.js
+++ client/src/components/LandingHero/index.js
@@ -9,38 +9,38 @@ function LandingHero() {
             <div className="bd-example">
             <div id="landingCarousel" className="carousel slide container" data-ride="carousel">
                 <ol className="carousel-indicators">
-                    <li data-target="#carouselExampleCaptions" data-slide-to="0" className="active"></li>
-                    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                    <li data-target="#landingCarousel" data-slide-to="0" className="active"></li>
+                    <li data-target="#landingCarousel" data-slide-to="1"></li>
+                    <li data-target="#landingCarousel" data-slide-to="2"></li>
                 </ol>
                 <div className="carousel-inner">
                     <div className="carousel-item active">
-                        <img src="./Imgs/raoul-croes-1093739-unsplash1.jpg" className="d-block w-100 landing-carousel" alt="..." />
+                        <img src="./Imgs/raoul-croes-1093739-unsplash1.jpg" className="d-block w-100" alt="..." />
                         <div className="carousel-caption d-none d-md-block">
-                            <h5>First slide label</h5>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            <h5>Get Inspired</h5>
+                            <p>Find professional photos that inspire your next photoshoot.</p>
                         </div>
                     </div>
                 <div className="carousel-item">
-                    <img src="..." className="d-block w-100" alt="..." />
+                    <img src="./Imgs/olivia-hutcherson-1331720-unsplash1.jpg" className="d-block w-100" alt="..." />
                     <div className="carousel-caption d-none d-md-block">
-                    <h5>Second slide label</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <h5>Visualize and Plan</h5>
+                        <p>Create a flexible mood board, set prop and equipment lists, and more.</p>
                     </div>
                 </div>
                 <div className="carousel-item">
-                    <img src="..." className="d-block w-100" alt="..." />
+                    <img src="./Imgs/jed-dela-cruz-505819-unsplash1.jpg" className="d-block w-100" alt="..." />
                     <div className="carousel-caption d-none d-md-block">
-                    <h5>Third slide label</h5>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        <h5>Share Your Work</h5>
+                        <p>Create a free gallery to showcase your hardwork, and add links to your social media or portfolio.</p>
                     </div>
                 </div>
                 </div>
-                <a className="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                <a className="carousel-control-prev" href="#landingCarousel" role="button" data-slide="prev">
                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span className="sr-only">Previous</span>
                 </a>
-                <a className="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                <a className="carousel-control-next" href="#landingCarousel" role="button" data-slide="next">
                 <span className="carousel-control-next-icon" aria-hidden="true"></span>
                 <span className="sr-only">Next</span>
                 </a>
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 // import { Link } from "react-router-dom";
 import LandingHero from "../components/LandingHero";
+import landingImgs from "../../src/landingInfoImgs.json";
 
 function Landing() {
     return (
